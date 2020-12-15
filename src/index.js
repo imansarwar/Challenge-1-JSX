30013df8b8340cae0ad85bb3a1e37e4b9828eb9b
Syeda Iman Sarwar <69895631+imansarwar@users.noreply.github.com>
2020-12-15 16:12:39 +0500

    Commit changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
+const currentDate = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString();
+
 ReactDOM.render(
   <>
     <h1 className="head">Iman Sarwar Netflix Picks</h1>
+    <p> Current Date = {currentDate}</p>
+    <p> Current Date = {currentTime}</p>
     <div className="body">
       <p className="text">Here are my picks for the Netflix Movies</p>
       <ol>
